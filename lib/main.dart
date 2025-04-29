e1d7b6ac521d9c686f186785f98c436810f2c8b6
Sabir Ullah <sabirullah.okz@gmail.com>
2025-04-30 00:11:40 +0500

    Network image + local image + singleChildScrollView + Padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meet_with_dev/24_april_task.dart';
 import 'package:meet_with_dev/row_practise.dart';
+import 'package:meet_with_dev/twentyeight_april_class.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: RowPractise(),
+    return MaterialApp(home: TwentyeightAprilClass(),
     debugShowCheckedModeBanner: false,
     );
   }
--- lib/twentyeight_april_class.dart
+++ lib/twentyeight_april_class.dart
@@ -5,6 +5,39 @@ class TwentyeightAprilClass extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [],),);
+    return Scaffold(
+      appBar: AppBar(title: Text("28 April 2025"),backgroundColor: Colors.amber,),
+      body: SingleChildScrollView(
+        child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          spacing: 20,
+          children: [
+          Container(
+            height: 200,width: 500,
+             decoration: BoxDecoration(
+               color: const Color(0xff7c94b6),
+               border: Border.all(
+                 width: 8,
+                   color: Colors.yellow
+                   // color: const Color(0xFF000000)
+               ),
+               borderRadius: BorderRadius.circular(50),
+             ),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",height: 100,width: 300,fit: BoxFit.cover,)),
+           ),
+        
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+              child: Image.network("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",height: 300,width: 500,fit: BoxFit.cover,)),
+        
+            ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset("assets/images/WhatsApp Image 2025-04-29 at 12.48.31_ebcdf53a.jpg",height: 500,width: 500,fit: BoxFit.cover,)),
+        ],),
+            ),
+      ),);
   }
 }
