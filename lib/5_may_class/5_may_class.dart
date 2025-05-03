c332c8503071eae58bec96e51c6cc00c4634528c
Sabir Ullah <sabirullah.okz@gmail.com>
2025-05-03 23:17:35 +0500

    Text Fields practise

---
+++ lib/5_may_class/5_may_class.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class FiveMayClass extends StatelessWidget {
+  const FiveMayClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+      backgroundColor: Colors.orange,
+      title: Text(
+        "Text Fields",
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+      ),
+      leading: Icon(Icons.menu,color: Colors.white,),
+      actions: [
+        Icon(Icons.add,color: Colors.white,),
+        Icon(Icons.minimize,color: Colors.white,),
+        SizedBox(width: 30,)
+      ],
+    ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 30,
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("assets/images/WhatsApp Image 2025-04-29 at 12.48.31_ebcdf53a.jpg",height: 300,width: 500,fit: BoxFit.cover,)),
+                Center(child: Text("Login Form", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.orange),)),
+
+                /// Login
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 8,
+                  children: [
+                  Text("Enter your email",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange,width: 2),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey,width: 2),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey,width: 2),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange,width: 2),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: "Enter Your Email Address",
+                      prefixIcon: Icon(Icons.email),
+
+                    ),
+                  ),
+                ],),
+
+                /// Password
+                Column(
+                  spacing: 8,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text("Enter your password",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.orange,width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        disabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey,width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey,width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.orange,width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        hintText: "Type password",
+                        prefixIcon: Icon(Icons.password),
+                        suffixIcon: Icon(Icons.visibility_off)
+
+                    ),
+                  ),
+                ],),
+
+                Center(child: ElevatedButton(onPressed: (){}, child: Text('Login'))),
+              ]
+          ),
+        ),
+      ),);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meet_with_dev/30_april_class/30_april_class.dart';
+import 'package:meet_with_dev/5_may_class/5_may_class.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ThirtyAprilClass(),
+    return MaterialApp(home: FiveMayClass(),
     debugShowCheckedModeBanner: false,
     );
   }
