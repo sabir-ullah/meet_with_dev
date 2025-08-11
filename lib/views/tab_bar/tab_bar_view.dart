91f5d882fb40423cb0469ef4d816cde05cc63909
Sabir Ullah <sabirullah.okz@gmail.com>
2025-08-11 23:55:33 +0500

    packages

--- lib/views/tab_bar/tab_bar_view.dart
+++ lib/views/tab_bar/tab_bar_view.dart
@@ -1,4 +1,6 @@
+import 'package:debit_credit_card_widget/debit_credit_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class TabBarViewPractise extends StatefulWidget {
   const TabBarViewPractise({super.key});
@@ -10,6 +12,13 @@ class TabBarViewPractise extends StatefulWidget {
 class _TabBarViewPractiseState extends State<TabBarViewPractise> with TickerProviderStateMixin{
    late TabController tabController;
    int index = 0;
+   List<_SalesData> data = [
+     _SalesData('Jan', 35),
+     _SalesData('Feb', 28),
+     _SalesData('Mar', 34),
+     _SalesData('Apr', 32),
+     _SalesData('May', 40),
+   ];
    @override
   void initState() {
     // TODO: implement initState
@@ -42,7 +51,41 @@ class _TabBarViewPractiseState extends State<TabBarViewPractise> with TickerProv
               children: [
             Center(child: Text("Tab 1")),
             Center(child: Text("Tab 2")),
-            Center(child: Text("Tab 3")),
+      Center(
+        child: DebitCreditCardWidget(
+          cardType: CardType
+              .debit, // CAN SET CARD TYPE AS DEBIT,CREDIT OR CUSTOM(FOR CUSTOM USE :- customCardType )
+          cardBrand: CardBrand
+              .mastercard, // CAN SET CARD BRAND (VISA,MASTERCARD,AMERICAN EXPRESS , DISCOVER, RUPAY OR CUSTOM(FOR CUSTOM USE :- cardBrandImage))
+          cardExpiry: "2140", // CARD EXPIRY DATE
+          color1: Colors.purple, // GRADIENT FIRST COLOR
+          showNFC: false, // BOOL TO SHOW OR HIDE NFC LOGO
+          cardNumber: "1234123412341234", // CARD NUMBER ONLY 16 CHARACTERS
+          cardHolderName: "Vaibhav Chandolia", // CARD HOLDER NAME
+          backgroundDecorationImage: DecorationImage(
+              image: AssetImage("assets/images/background.png")), // YOU CAN ADD BACKGROUND IMAGE FROM HERE
+        ),
+    ),
+
+                SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  // Chart title
+                  title: ChartTitle(text: 'Half yearly sales analysis'),
+                  // Enable legend
+                  legend: Legend(isVisible: true),
+                  // Enable tooltip
+                  tooltipBehavior: TooltipBehavior(enable: true),
+                  series: <CartesianSeries<_SalesData, String>>[
+                    LineSeries<_SalesData, String>(
+                      dataSource: data,
+                      xValueMapper: (_SalesData sales, _) => sales.year,
+                      yValueMapper: (_SalesData sales, _) => sales.sales,
+                      name: 'Sales',
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true),
+                    ),
+                  ],
+                ),
           ]),
         )
       ],),
