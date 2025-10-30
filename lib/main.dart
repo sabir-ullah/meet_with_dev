cde91593e61cbeb242bdf572b8e44839af5b5adb
Sabir Ullah <sabirullah.okz@gmail.com>
2025-10-31 00:49:00 +0500

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:meet_with_dev/firebase_options.dart';
 import 'package:meet_with_dev/views/doctor_module/doctor_list_view.dart';
 import 'package:meet_with_dev/views/doctor_module/std_task/doctor_std_task.dart';
 import 'package:meet_with_dev/views/grocery_app_task/grocery_app_task.dart';
 import 'package:meet_with_dev/views/home_task/home_task.dart';
-import 'package:meet_with_dev/views/oop_dart/class_classes/class_classes.dart';
 import 'package:meet_with_dev/views/std_task_grad_style/std_task_grad_style.dart';
 import 'package:meet_with_dev/views/tab_bar/bottom_bar_view.dart';
 import 'package:meet_with_dev/views/tab_bar/tab_bar_view.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Vehical();
   runApp(
       // DevicePreview(
       // enabled: true,
       // builder: (context) =>  MyApp()));
+
+
+// ...
+
+
       MyApp());
 }
 
